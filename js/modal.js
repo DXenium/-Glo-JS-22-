ffa903ab14c1a314dad621c60426f154b4edd341
DudakXen <dudak.xen@yandex.ru>
2022-05-05 16:30:20 +0300

    Добавлен крестик для закрытия модального окна

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,39 @@
 const modalBtn = document.querySelector('.modal__button')
 const smallBtn = document.querySelector('.course__button')
 const modal = document.querySelector('.modal')
+const modalInner = modal.querySelector('.modal__inner')
 
+modalInner.style.position = 'relative'
+
+//добавляем крестик для закрытия модального окна
+const addCloseBtn = () => {
+    const closeBtn = document.createElement('div')
+
+    closeBtn.classList.add('close-button')
+    closeBtn.innerHTML = '&times'
+    closeBtn.style.position = 'absolute'
+    closeBtn.style.right = '10px'
+    closeBtn.style.top = '10px'
+    closeBtn.style.width = '20px'
+    closeBtn.style.height = '20px'
+    closeBtn.style.fontSize = '26px'
+    closeBtn.style.cursor = 'pointer'
+    closeBtn.style.display = 'flex'
+    closeBtn.style.justifyContent = 'center'
+    closeBtn.style.alignItems = 'center'
+
+    modalInner.append(closeBtn)
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = ''
+    })
+}
+
+//открываем модальное окно по клику на большую кнопку
 modalBtn.addEventListener('click', () => {
     modal.style.display = 'flex'
 })
-
+//открываем модальное окно по клику на маленькую кнопку
 smallBtn.addEventListener('click', () => {
     modal.style.display = 'flex'
 })
@@ -17,3 +45,5 @@ modal.addEventListener('click', (event) => {
         modal.style.display = ''
     }
 })
+
+addCloseBtn()
